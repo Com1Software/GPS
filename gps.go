ddd6c3a7747e17c6ffa9c8a1d5e63f88d25c2c7e
Com1 Software, Inc <information@com1software.com>
2022-11-20 11:21:03 -0500

    Update gps.go

--- gps.go
+++ gps.go
@@ -2,9 +2,13 @@ package gps
 
 import "fmt"
 
-// Hello returns a greeting for the named person.
 func Hello(name string) string {
-    // Return a greeting that embeds the name in a message.
     message := fmt.Sprintf("Hi, %v. Welcome!", name)
     return message
+}
+
+
+func GetSerialPort() string {
+    port := "com1"
+    return port
 }
